b26d352461681c5ae5258d503528cb145eac9462
Braden Mabey <braden.mabey@gmail.com>
2025-11-17 16:56:57 -0500

    Mostly finished the layout.

--- tmdb-cli.py
+++ tmdb-cli.py
@@ -6,12 +6,21 @@ class TMDB_App(App):
     CSS_PATH = "tmdb.tcss"
     BINDINGS = [
         ("d", "toggle_dark", "Toggle Dark Mode"),
+        ("s", "search", "Search for a movie"),
         ("q", "quit", "Quit")
         ]
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
         yield Footer()
+        yield Label("Popular", id="pop")
+        yield Label("Now Playing", id="now")
+        yield Label("Top Rated", id="top")
+        yield Label("Upcoming", id="up")
+        yield ListView(ListItem(Label("test1")))
+        yield ListView(ListItem(Label("test2")))
+        yield ListView(ListItem(Label("test3")))
+        yield ListView(ListItem(Label("test4")))
         yield TextArea(placeholder="Search...", compact=True)
 
 if __name__ == "__main__":
